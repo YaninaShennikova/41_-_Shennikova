dddc634f649eb0598cc0c53c38df696efcd0ccd7
Vadim <Vadim@DESKTOP-L9UBP2R>
2025-03-04 11:21:12 +0500

    Добавьте файлы проекта.

---
+++ AuthPage.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _41_размер_Shennikova
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        private int failedAttempts = 0;
+        private string generatedCaptcha = "";
+
+        public AuthPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ButtonLоgin_Click(object sender, RoutedEventArgs e)
+        {
+            string login = Login.Text.Trim();
+            string password = Password.Text.Trim();
+
+            if (string.IsNullOrEmpty(login) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Есть пустые поля!");
+                return;
+            }
+
+            // Проверка логина и пароля в БД
+            User user = Shennikova41Entities.GetContext().User.FirstOrDefault(p => p.UserLogin == login && p.UserPassword == password);
+
+            if (user != null)
+            {
+                MessageBox.Show("Успешный вход!");
+                Manager.MainFrame.Navigate(new ProductPage(user));
+                ResetAuthForm();
+            }
+            else
+            {
+                failedAttempts++;
+
+                if (failedAttempts == 1)
+                {
+                    MessageBox.Show("Введены неверные данные! Введите капчу.");
+                    GenerateCaptcha();
+                    CaptchaPanel.Visibility = Visibility.Visible;
+                }
+                else if (failedAttempts >= 2)
+                {
+                    if (CaptchaInput.Text.Trim() != generatedCaptcha)
+                    {
+                        MessageBox.Show("Капча введена неверно! Блокировка входа на 10 секунд.");
+                        BlockLoginButton();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Успешная проверка капчи, но логин или пароль неверны!");
+                    }
+                }
+            }
+        }
+
+        private void GenerateCaptcha()
+        {
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            Random rand = new Random();
+            generatedCaptcha = new string(Enumerable.Range(0, 4).Select(_ => chars[rand.Next(chars.Length)]).ToArray());
+
+            // Устанавливаем символы в TextBlock и меняем их позиции
+            captchaOne.Text = generatedCaptcha[0].ToString();
+            captchaTwo.Text = generatedCaptcha[1].ToString();
+            captchaThree.Text = generatedCaptcha[2].ToString();
+            captchaFour.Text = generatedCaptcha[3].ToString();
+
+         
+        }
+
+        private async void BlockLoginButton()
+        {
+            ButtonLоgin.IsEnabled = false;
+            await Task.Delay(10000); // Блокировка на 10 секунд
+            ButtonLоgin.IsEnabled = true;
+        }
+        private void ResetAuthForm()
+        {
+            Login.Clear();
+            Password.Clear();
+            CaptchaInput.Clear();
+            CaptchaPanel.Visibility = Visibility.Collapsed;
+            failedAttempts = 0;
+        }
+        private void Gost_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new ProductPage(null));
+            ResetAuthForm();
+        }
+    }
+}
---
+++ ProductPage.xaml.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _41_размер_Shennikova
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductPage.xaml
+    /// </summary>
+    public partial class ProductPage : Page
+    {
+
+        int CountRecords;//Количество записей в таблице
+        int CountPage;//Общее количество страниц
+        int CurrentPage = 0;//Текущая страница
+
+        List<Product> CurrentPageList = new List<Product>();
+        List<Product> TableList;
+
+        private User _user;
+        public ProductPage(User user)
+        {
+            string logi;
+            string rol;
+            InitializeComponent();
+            _user = user;
+            var currentProducts = Shennikova41Entities.GetContext().Product.ToList();
+            ProductListView.ItemsSource = currentProducts;
+            if (user != null)
+            {
+                logi = user.UserSurname + user.UserName + user.UserPatronymic;
+                Imya.Text = logi;
+                switch (user.UserRole)
+                {
+                    case 1:
+                        rol = "Клиент";
+                        break;
+                    case 2:
+                        rol = "Менеджер";
+                        break;
+                    case 3:
+                        rol = "Администратор";
+                        break;
+                    default:
+                        rol = "Гость";
+                        break;
+
+                }
+            }
+            else
+            {
+                rol = "Гость";
+            }
+
+            Role.Text = rol;
+
+
+            UpdateProduct();
+        }
+
+        
+        
+
+        private void UpdateProduct()
+
+        {
+            
+            var currentProducts = Shennikova41Entities.GetContext().Product.ToList();
+
+            
+
+            if (ComboType.SelectedIndex == 0)
+            {
+                currentProducts = currentProducts.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 0 && Convert.ToInt32(p.ProductMaxDiscount) <= 100)).ToList();
+            }
+
+            if (ComboType.SelectedIndex == 1)
+            {
+                currentProducts = currentProducts.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 0 && Convert.ToInt32(p.ProductMaxDiscount) < 9.99)).ToList();
+            }
+            if (ComboType.SelectedIndex == 2)
+            {
+                currentProducts = currentProducts.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 10 && Convert.ToInt32(p.ProductMaxDiscount) < 14.99)).ToList();
+            }
+            if (ComboType.SelectedIndex == 3)
+            {
+                currentProducts = currentProducts.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 15 && Convert.ToInt32(p.ProductMaxDiscount) < 100)).ToList();
+            }
+
+
+            currentProducts = currentProducts.Where(p => p.ProductName.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+            ProductListView.ItemsSource = currentProducts.ToList();
+
+            if (RButtonDown.IsChecked.Value)
+            {
+                currentProducts = currentProducts.OrderByDescending(p => p.ProductCost).ToList();
+            }
+            if (RButtonUp.IsChecked.Value)
+            {
+                currentProducts = currentProducts.OrderBy(p => p.ProductCost).ToList();
+
+            }
+
+
+            ProductListView.ItemsSource = currentProducts;
+            TableList = currentProducts;
+
+          
+            Count.Text = "Количество " + currentProducts.Count.ToString() + " из " + Shennikova41Entities.GetContext().Product.ToList().Count.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void ProductListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateProduct();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateProduct();
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateProduct();
+        }
+        
+
+        private void RButtonUp_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateProduct();
+        }
+
+        private void RButtonDown_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateProduct();
+        }
+    }
+}
